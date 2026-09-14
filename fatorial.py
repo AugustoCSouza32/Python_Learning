811d0734cb2f14cdcca79ccd6fb6969dcd9795e1
Augusto César de Souza <augustocesar16180@gmail.com>
2026-09-14 10:50:35 -0300

    Update fatorial.py
    coloca um laço de repetição enquanto o número for maior que 15 ou menor que 0.  Informando uma mensagem de erro.

--- fatorial.py
+++ fatorial.py
@@ -1,4 +1,4 @@
-# Fazer um programa para ler um número N, e depois calcular e mostrar o
+# Fazer um programa para ler um número N {n >= 0 e n <=15}, e depois calcular e mostrar o
 # fatorial de N.
 
 import sys
@@ -9,10 +9,16 @@ import sys
 sys.set_int_max_str_digits(300000)
 
 
+
 n = int(input("Digite o valor de N: "))
 
-j = n
+while n > 15 or n < 0:
+    print("Erro informe um número de 0 a 15")
+    n = int(input("Digite o valor de N: "))
 
+
+
+j = n
 if n == 0:
     f = 1
 else:
